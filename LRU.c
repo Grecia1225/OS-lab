1720b83f4613a149c9967148c26e2146ac4c3f52
Grecia 1225 <thellagrecia@email.com>
2026-01-16 12:42:31 +0530

    added LRU.c

--- LRU.c
+++ LRU.c
@@ -5,28 +5,42 @@ printf("Enter number of frames: ");
 scanf("%d", &frames);
 printf("Enter number of pages: ");
 scanf("%d", &pages);
-int frame[frames], page[pages];
+int frame[frames], page[pages], recent[frames];
 // Initialize frames to -1 (means empty)
 for(i = 0; i < frames; i++) {
 frame[i] = -1;
+recent[i] = 0; // No usage yet
 }
 printf("Enter the page reference string: ");
 for(i = 0; i < pages; i++) {
 scanf("%d", &page[i]);
 }
-int index = 0; // To replace frames in FIFO order
+int time = 0; // To track usage time
 for(i = 0; i < pages; i++) {
 int found = 0; // Flag to check if page hit
-// Check if page is already in any frame
+// Check if page is already present
 for(j = 0; j < frames; j++) {
 if(frame[j] == page[i]) {
 found = 1; // Page hit
+time++;
+recent[j] = time; // Update recent usage
 break;
 }
 }
-if(found == 0) { // Page fault occurs
-frame[index] = page[i]; // Replace frame at 'index'
-index = (index + 1) % frames; // Move to next frame (circular)
+if(found == 0) { // Page not found - page fault
+int lru_index = 0;
+int min_recent = recent[0];
+// Find least recently used frame
+for(j = 1; j < frames; j++) {
+if(recent[j] < min_recent) {
+min_recent = recent[j];
+lru_index = j;
+}
+}
+// Replace LRU page with new page
+frame[lru_index] = page[i];
+time++;
+recent[lru_index] = time; // Update recent usage
 page_faults++;
 }
 // Print current frame contents
